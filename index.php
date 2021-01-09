d873e1e9c5ed8d7f5bac396d20512931c4cf9890
ricardomoratomateos <ricardomoratomateos@outlook.com>
2021-01-09 18:43:28 +0100

    distinguish interactive/non-intectave with args

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+
+if ($argv[1] === '--interactive') {
+    require  __DIR__ . '/src/Infrastructure/Console/Interactive/index.php';
+} else {
+    require  __DIR__ . '/src/Infrastructure/Console/NonInteractive/index.php';
+}
