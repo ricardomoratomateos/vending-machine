d4227b3f4a5787509e8405c5e6291d2aac4788af
ricardomoratomateos <ricardomoratomateos@outlook.com>
2021-01-09 18:42:22 +0100

    remove unused files

--- docker/scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$(dirname "$(which "$0")")"
-
-source ${SCRIPT_DIR}/setEnvVars.sh
-
-cd ${SCRIPT_DIR}/../../
-docker-compose -p $PROJECT_NAME up -d
-cd -
--- docker/scripts/setEnvVars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$(dirname "$(which "$0")")"
-
-cd ${SCRIPT_DIR}/../../
-# Read and export .env variables ignoring lines that start with #
-export $(grep -v '^#' .env | xargs)
-cd -
