7c87e93a3cf08eaf0e3ad45009974994f69a8a7f
ricardomoratomateos <ricardomoratomateos@outlook.com>
2021-01-09 18:44:27 +0100

    refactor docker-composer files and composer commands

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 # Script for run dockers and execute tests inside them
 
-docker-compose -p 'vending-machine-test' -f docker-compose.test.yml up -d
-
 case "$1" in
     "unit")
-        docker-compose -p 'vending-machine-test' exec -T php ./vendor/bin/phpunit --testsuit unit
+        docker-compose -f docker-compose.test.yml run phpunit --testsuit unit
         ;;
     "integration")
-        docker-compose -p 'vending-machine-test' exec -T php ./vendor/bin/phpunit --testsuit integration
-        ;;
-    "coverage")
-        docker-compose -p 'vending-machine-test' exec -T php ./vendor/bin/phpunit --coverage-text
+        docker-compose -f docker-compose.test.yml run phpunit --testsuit integration
         ;;
     "code:style")
-        docker-compose -p 'vending-machine-test' exec -T php ./vendor/bin/phpcs --standard=PSR12 --colors ./src
+        docker-compose -f docker-compose.test.yml run phpcs --standard=PSR12 --colors ./src
         ;;
     "fix:code:style")
-        docker-compose -p 'vending-machine-test' exec -T php ./vendor/bin/phpcbf --standard=PSR12 --colors ./src
+        docker-compose -f docker-compose.test.yml run phpcbf --standard=PSR12 --colors ./src
         ;;
     *)
-        docker-compose -p 'vending-machine-test' exec -T php ./vendor/bin/phpunit
+        docker-compose -f docker-compose.test.yml run phpunit
         ;;
     esac
-
-docker-compose -p 'vending-machine-test' stop
